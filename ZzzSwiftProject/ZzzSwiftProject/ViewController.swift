68891f3192cb5439fed1dfc95ac15e0099d16fbe
zhouzezhou <zhouze4zhou33@sina.com>
2018-05-02 09:57:21 +0800

    creat swift language study projectand print hello world console text .

---
+++ ZzzSwiftProject/ZzzSwiftProject/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ZzzSwiftProject
+//
+//  Created by zhouzezhou on 2018/5/2.
+//  Copyright © 2018年 zzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello world !")
+        NSLog("hello world !")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
